f66c91888e001124ab53a9bd940f6f853137cfdf
Luiz Felipe <lflimeira02@gmail.com>
2018-05-06 16:32:17 -0300

    test: adds title, participe and projetos e2e tests

--- cypress/integration/tc_spec.js
+++ cypress/integration/tc_spec.js
@@ -1,7 +1,50 @@
-describe('Kitchen Sink', function () {
-  it('.should() - assert that <title> is correct', function () {
-    cy.visit('https://example.cypress.io')
+describe('Training Center - Site', function () {
+  describe('Title', function () {
+    it('should assert that title is correct', function () {
+      cy.visit('https://trainingcenter.io/')
 
-    cy.title().should('include', 'Kitchen Sink')
+      cy.get('.main-content__title')
+        .contains('Training Center')
+    })
+  })
+
+  describe('Participe', function () {
+    it('should assert that the item Participe exist on menu', function () {
+      cy.contains('Participe').click()
+    })
+    
+    it('should ensure that the Participe item has slack and github link', function () {
+      cy.url().should('include', '#come')
+
+      cy.get('.come__title')
+        .contains('Participe da nossa comunidade')
+
+      const participe = '.come__nav'
+
+      cy.contains(participe, '-> Chat no Slack')
+      cy.contains(participe, '-> Forum no GitHub')
+    })
+  })
+
+  describe('Projetos', function () {
+    it('should assert that the item Projetos exist on menu', function () {
+      cy.contains('Projetos').click()
+    })
+    
+    it('should ensure that the Projetos item has all projects', function () {
+      cy.url().should('include', '#initiatives')
+
+      cy.get('.initiatives__box__title')
+        .contains('Nossas iniciativas')
+      
+      const projects = '.initiatives__items'
+      
+      cy.contains(projects, 'Mentoria')
+      cy.contains(projects, 'Grupos de estudos')
+      cy.contains(projects, 'Revisão de código')
+      cy.contains(projects, 'Encontros locais')
+    })
   })
 })
+
+
