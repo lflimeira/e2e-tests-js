f38a801da736aeddf559daffac13af8034b2d2a7
Luiz Felipe <lflimeira02@gmail.com>
2018-05-08 02:40:50 -0300

    tests: adds blog and apoie e2e tests

--- cypress/integration/tc_spec.js
+++ cypress/integration/tc_spec.js
@@ -21,8 +21,8 @@ describe('Training Center - Site', function () {
 
       const participe = '.come__nav'
 
-      cy.contains(participe, '-> Chat no Slack')
-      cy.contains(participe, '-> Forum no GitHub')
+      cy.contains(participe, 'chat (Slack)')
+      cy.contains(participe, 'encontros presenciais regionais')
     })
   })
 
@@ -43,6 +43,20 @@ describe('Training Center - Site', function () {
       cy.contains(projects, 'Grupos de estudos')
       cy.contains(projects, 'Revisão de código')
       cy.contains(projects, 'Encontros locais')
+    })
+  })
+
+  describe('Blog', () => {
+    it('should ensure that the Blog item is linked with the correctly url', function () {
+      cy.get('.nav-menu__list > li:nth-child(4) > a')
+        .should('have.attr', 'href').and('include', 'https://medium.com/trainingcenter')
+    })
+  })
+
+  describe('Apoie', () => {
+    it('should ensure that the Apoie item is linked with the correctly url', function () {
+      cy.get('.nav-menu__list > li:nth-child(1) > a')
+        .should('have.attr', 'href').and('include', 'https://apoia.se/training-center')
     })
   })
 })
